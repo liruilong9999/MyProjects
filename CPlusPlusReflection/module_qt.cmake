bd49b3ad321908086856ac78b075a5620412f983
liruilong9999 <1355165054@qq.com>
2023-05-25 21:03:50 +0800

    修改EventBus 1.增加线程安全 2.增加事件处理顺序：事件处理是按照事件队列中的顺序进行处理的，可能存在某些事件长时间未被处理的情况，导致其他事件得不到及时处理。 3.增加错误处理机制：在事件发布和处理过程中，没有提供错误处理机制，可能会导致未处理的异常或错误不被捕获和处理。 4.增加事件代码管理：事件代码是使用整数表示的，需要保证不同模块之间使用的事件代码不会冲突，否则可能导致错误的事件处理。

--- CPlusPlusReflection/module_qt.cmake
+++ CPlusPlusReflection/module_qt.cmake
@@ -1,12 +1,14 @@
 # 设置 qt 目录
-if(CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
+if(CMAKE_CXX_PLATFORM_ID MATCHES "Windows")
     # 方法一
     set(Qt5_DIR "C:\\ThirdPart\\Qt5.14\\lib\\cmake\\Qt5")
     # 方法二
     # set(CMAKE_PREFIX_PATH "C:\\Qt\\Qt5.14.2\\5.14.2\\msvc2017_64")
-else()
+elseif(CMAKE_CXX_PLATFORM_ID MATCHES "MinGW")
     set(CMAKE_PREFIX_PATH "C:\\ThirdPart\\Qt5.14\\lib\\cmake\\Qt5")
+elseif(CMAKE_CXX_PLATFORM_ID MATCHES "Linux")
 endif()
+
 # 自动生成
 set(CMAKE_INCLUDE_CURRENT_DIR ON)
 set(CMAKE_AUTOMOC ON)
@@ -14,40 +16,19 @@ set(CMAKE_AUTOUIC ON)
 set(CMAKE_AUTORCC ON)
 
 # ARGV0 QT 依赖文件
-# ARGV1 UI 文件
-# ARGV2 QRC 文件
-macro(AddQtInc)
+macro(AddQtInc QtLibraryList)
     # 添加 qt include
-    if(${ARGC} GREATER_EQUAL 1)
-        foreach(qt_library ${ARGV0})
-            find_package(Qt5 COMPONENTS ${qt_library} REQUIRED)
-            include_directories(${Qt5${qt_library}_INCLUDE_DIRS})
-            include_directories(${Qt5${qt_library}_PRIVATE_INCLUDE_DIRS})
-        endforeach()
-    endif()
-
-    # 编译 ui 文件
-    if(${ARGC} GREATER_EQUAL 2)
-        qt5_wrap_ui(FORM_FILES ${ARGV1})
-    endif()
-
-    # 编译 ui 文件
-    if(${ARGC} GREATER_EQUAL 3)
-        qt5_add_resources(RESOURCE_FILES "${ARGV2}")
-    endif()
-endmacro()
-
-macro(AddQtLib)
-    # 添加 lib
-    foreach(qt_library ${ARGV0})
-        link_directories(${QTDIR}/lib)
-        target_link_libraries(${PROJECT_NAME} Qt5::${qt_library})
+    foreach(qt_library ${QtLibraryList})
+        find_package(Qt5 COMPONENTS ${qt_library} REQUIRED)
+        include_directories(${Qt5${qt_library}_INCLUDE_DIRS})
+        include_directories(${Qt5${qt_library}_PRIVATE_INCLUDE_DIRS})
     endforeach()
 endmacro()
 
-macro(QtCopy)
-    # 拷贝文件
-    execute_process(COMMAND ${CMAKE_COMMAND} -E copy_directory 
-        ${QTDIR}/plugins/platforms 
-        ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/Debug/platforms)
+macro(AddQtLib QtLibraryList)
+    # 添加 lib
+    foreach(qt_library ${QtLibraryList})
+        link_directories(${QTDIR}/lib)
+        target_link_libraries(${PROJECT_NAME} Qt5::${qt_library})
+    endforeach()
 endmacro()
--- CPlusPlusReflection/src/LBaseCommon/LEventBus.h
+++ CPlusPlusReflection/src/LBaseCommon/LEventBus.h
@@ -1,72 +1,165 @@
 #pragma once
 
+#include <condition_variable>
 #include <functional>
+#include <mutex>
+#include <queue>
+#include <thread>
 #include <unordered_map>
 #include <vector>
 
-#include "LSingleton.h"
-
-class LBASECOMMON_EXPORT EventBus:public LSingleton<EventBus>
+class EventBus
 {
-	friend class LSingleton<EventBus>;
 public:
     struct CallbackItem
     {
-		//用于存储回调函数的相关信息，包括回调函数的优先级、函数指针等。
-        std::function<void(int, void*, int)> callback;  // 回调函数
-        int                                  priority;  // 优先级
-        void*                                rawObject; // 对象指针，用于根据对象取消订阅事件
+        std::function<void(int, void*, int)> callback;
+        int                                  priority;
+        void*                                rawObject;
     };
 
     struct EventCallbacksItem
     {
-		//用于存储事件和对应的回调函数列表。
-        std::vector<CallbackItem> callbacks; // 回调函数列表
+        std::vector<CallbackItem> callbacks;
+        std::mutex                mutex; // 用于保护回调函数列表的互斥锁
     };
 
     struct EventQueueItem
     {
-		//表示待处理的事件项，包含事件的代码、参数、订阅者列表等。
-        int                       eventCode;  // 事件代码
-        void*                     data;       // 参数
-        int                       receiverId; // 接收者ID
-        std::vector<CallbackItem> callbacks;  // 回调函数列表
+        int                       eventCode;
+        void*                     data;
+        int                       receiverId;
+        std::vector<CallbackItem> callbacks;
     };
 
     struct StickyEvent
     {
-		//用于存储粘性事件的信息，粘性事件是一种特殊的事件，它会一直保留在事件队列中，直到被处理。
-        int   eventCode;  // 事件代码
-        void* data;       // 参数
-        int   receiverId; // 接收者ID
+        int   eventCode;
+        void* data;
+        int   receiverId;
     };
 
-    // 发布一个事件,将事件存储在事件队列中
     void post(int eventCode, void* data, int receiverId);
 
-    // 订阅一个事件
     void subscribe(int eventCode, std::function<void(int, void*, int)> callback, int priority = 0, void* rawObject = nullptr);
 
-    // 取消订阅一个事件
     void unsubscribe(int eventCode, std::function<void(int, void*, int)> callback);
 
-    // 根据对象取消订阅事件
     void unsubscribeByRawObject(void* rawObject);
 
-    // 处理事件队列中的事件
     void handlePost();
 
-    // 发布一个粘性事件
     void postStickyEvent(int eventCode, void* data, int receiverId);
 
-    // 处理粘性事件
     void handleStickyEvents();
 
 private:
-    std::unordered_map<int, EventCallbacksItem> eventCallbacks; // 存储事件和对应的回调函数列表
-    std::vector<EventQueueItem>                 eventQueue;     // 存储待处理的事件队列
-    std::unordered_map<int, StickyEvent>        stickyEvents;   // 存储粘性事件的信息
+    std::unordered_map<int, EventCallbacksItem> eventCallbacks;
+    std::queue<EventQueueItem>                  eventQueue;
+    std::unordered_map<int, StickyEvent>        stickyEvents;
+    std::mutex                                  eventQueueMutex; // 用于保护事件队列的互斥锁
+    std::condition_variable                     eventQueueCV;    // 用于事件队列的条件变量，用于唤醒等待处理事件的线程
 
-    // 处理事件队列中的事件项
     void handleEvents(const EventQueueItem& eventQueueItem);
 };
+
+// 发布一个事件
+void EventBus::post(int eventCode, void* data, int receiverId)
+{
+    std::lock_guard<std::mutex> lock(eventQueueMutex);
+    auto                        eventCallbacksItem = eventCallbacks.find(eventCode);
+    if (eventCallbacksItem != eventCallbacks.end())
+    {
+        EventQueueItem eventQueueItem = {eventCode, data, receiverId, eventCallbacksItem->second.callbacks};
+        eventQueue.push(eventQueueItem);
+        eventQueueCV.notify_all(); // 唤醒等待处理事件的线程
+    }
+}
+
+// 订阅一个事件
+void EventBus::subscribe(int eventCode, std::function<void(int, void*, int)> callback, int priority, void* rawObject)
+{
+    std::lock_guard<std::mutex> lock(eventCallbacks[eventCode].mutex);
+    CallbackItem                callbackItem = {callback, priority, rawObject};
+    eventCallbacks[eventCode].callbacks.push_back(callbackItem);
+    std::sort(eventCallbacks[eventCode].callbacks.begin(), eventCallbacks[eventCode].callbacks.end(), [](const CallbackItem& a, const CallbackItem& b)
+              { return a.priority > b.priority; });
+}
+
+// 取消订阅一个事件
+void EventBus::unsubscribe(int eventCode, std::function<void(int, void*, int)> callback)
+{
+    std::lock_guard<std::mutex> lock(eventCallbacks[eventCode].mutex);
+    auto&                       callbacks = eventCallbacks[eventCode].callbacks;
+    callbacks.erase(std::remove_if(callbacks.begin(), callbacks.end(), [&](const CallbackItem& item)
+                                   { return item.callback.target_type() == callback.target_type(); }),
+                    callbacks.end());
+    if (callbacks.empty())
+    {
+        eventCallbacks.erase(eventCode);
+    }
+}
+
+// 根据对象取消订阅事件
+void EventBus::unsubscribeByRawObject(void* rawObject)
+{
+    std::lock_guard<std::mutex> lock(eventQueueMutex);
+    for (auto& eventCallbacksItem : eventCallbacks)
+    {
+        std::lock_guard<std::mutex> lock(eventCallbacksItem.second.mutex);
+        auto&                       callbacks = eventCallbacksItem.second.callbacks;
+        callbacks.erase(std::remove_if(callbacks.begin(), callbacks.end(), [&](const CallbackItem& item)
+                                       { return item.rawObject == rawObject; }),
+                        callbacks.end());
+        if (callbacks.empty())
+        {
+            eventCallbacks.erase(eventCallbacksItem.first);
+        }
+    }
+}
+
+// 处理事件队列中的事件
+void EventBus::handlePost()
+{
+    while (true)
+    {
+        EventQueueItem eventQueueItem;
+        {
+            std::unique_lock<std::mutex> lock(eventQueueMutex);
+            eventQueueCV.wait(lock, [&]
+                              { return !eventQueue.empty(); }); // 等待事件队列不为空
+            eventQueueItem = eventQueue.front();
+            eventQueue.pop();
+        }
+        handleEvents(eventQueueItem);
+    }
+}
+
+// 处理事件队列中的事件项
+void EventBus::handleEvents(const EventQueueItem& eventQueueItem)
+{
+    std::lock_guard<std::mutex> lock(eventCallbacks[eventQueueItem.eventCode].mutex);
+    for (const CallbackItem& callbackItem : eventQueueItem.callbacks)
+    {
+        callbackItem.callback(eventQueueItem.eventCode, eventQueueItem.data, eventQueueItem.receiverId);
+    }
+}
+
+// 发布一个粘性事件
+void EventBus::postStickyEvent(int eventCode, void* data, int receiverId)
+{
+    std::lock_guard<std::mutex> lock(eventQueueMutex);
+    StickyEvent                 stickyEvent = {eventCode, data, receiverId};
+    stickyEvents[eventCode]                 = stickyEvent;
+    post(eventCode, data, receiverId);
+}
+
+// 处理粘性事件
+void EventBus::handleStickyEvents()
+{
+    std::lock_guard<std::mutex> lock(eventQueueMutex);
+    for (auto& stickyEvent : stickyEvents)
+    {
+        handleEvents({stickyEvent.second.eventCode, stickyEvent.second.data, stickyEvent.second.receiverId, eventCallbacks[stickyEvent.second.eventCode].callbacks});
+    }
+}
